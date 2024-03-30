95265c66e3e126c57388c5a5f7fafa1bafc34a02
Stavros <steveiliop56@gmail.com>
2024-03-30 21:32:23 +0200

    wip

--- src/app/components/render-shell/components/info-dialog/info-dialog.tsx
+++ src/app/components/render-shell/components/info-dialog/info-dialog.tsx
@@ -1,15 +1,24 @@
 "use client";
 
 import { containerData } from "@/server/types/types";
-import { InfoCircledIcon } from "@radix-ui/react-icons";
-import { Button, Dialog, Flex, Text, Code } from "@radix-ui/themes";
+import { InfoCircledIcon, Pencil1Icon } from "@radix-ui/react-icons";
+import {
+  Button,
+  Dialog,
+  Flex,
+  Text,
+  Code,
+  Popover,
+  IconButton,
+  TextArea,
+} from "@radix-ui/themes";
 import React from "react";
 
-interface props {
+interface shellData {
   shell: containerData;
 }
 
-export const InfoDialog: React.FC<props> = ({ shell }) => {
+export const InfoDialog: React.FC<shellData> = ({ shell }) => {
   return (
     <Dialog.Root>
       <Dialog.Trigger>
@@ -42,12 +51,7 @@ export const InfoDialog: React.FC<props> = ({ shell }) => {
               {shell.distro}
             </Code>
           </Text>
-          <Text weight="medium">
-            Password:{" "}
-            <Code color="gray" variant="ghost">
-              {shell.password}
-            </Code>
-          </Text>
+          <PasswordEdit shell={shell} />
           <Text weight="medium">
             Port:{" "}
             <Code color="gray" variant="ghost">
@@ -71,5 +75,53 @@ export const InfoDialog: React.FC<props> = ({ shell }) => {
         </Flex>
       </Dialog.Content>
     </Dialog.Root>
+  );
+};
+
+const PasswordEdit: React.FC<shellData> = ({ shell }) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [isRequired, setIsRequired] = React.useState(true);
+  return (
+    <Flex className="flex-row gap-2">
+      <Text weight="medium">
+        Password:{" "}
+        <Code color="gray" variant="ghost">
+          {shell.password}
+        </Code>
+      </Text>
+
+      <Popover.Root open={isOpen}>
+        <Popover.Trigger>
+          <IconButton onClick={() => setIsOpen(true)} className="size-5">
+            <Pencil1Icon />
+          </IconButton>
+        </Popover.Trigger>
+
+        <Popover.Content>
+          <form onSubmit={() => setIsOpen(false)}>
+            <Flex className="flex-col gap-2">
+              <TextArea
+                required={isRequired}
+                placeholder="Type new password..."
+              />
+              <Flex className="flex-row gap-2 justify-end">
+                <Button
+                  color="gray"
+                  variant="soft"
+                  highContrast
+                  onClick={() => {
+                    setIsRequired(false);
+                    setIsOpen(false);
+                  }}
+                >
+                  Cancel
+                </Button>
+                <Button type="submit">Save</Button>
+              </Flex>
+            </Flex>
+          </form>
+        </Popover.Content>
+      </Popover.Root>
+    </Flex>
   );
 };
